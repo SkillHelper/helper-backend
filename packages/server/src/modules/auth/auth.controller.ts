6bf088f9fb338f6bde03e627e388c97bf261abeb
PleBea <bini060315kim@gmail.com>
2024-04-08 13:34:09 +0900

    feat: Add redirect

--- packages/server/src/modules/auth/auth.controller.ts
+++ packages/server/src/modules/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { DiscordGuard } from './guards/discord.guard';
 import { ApiTags } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('discord')
   @UseGuards(DiscordGuard)
@@ -14,5 +19,13 @@ export class AuthController {
 
   @Get('discord/callback')
   @UseGuards(DiscordGuard)
-  async discordCallback() {}
+  async discordCallback(@Res() res: Response) {
+    const frontendUrl = this.configService.get<string>('FRONTEND_URL');
+
+    if (!frontendUrl) {
+      throw new Error('FRONTEND_URL is not setted');
+    }
+
+    res.redirect(`${frontendUrl}`);
+  }
 }
